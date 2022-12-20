4b35cd4efaa7a33895e9215da77880f58508cd6e
Gokul Chandramohan <Gokul.Chandramohan@neudesic.com>
2022-12-20 10:47:06 +0530

    New Appium framework - C#

---
+++ AppiumDemo/DriverSetup.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace AppiumTest
+{
+    public class DriverSetup
+    {
+        AppiumDriver driver;
+        AppiumOptions cap;
+        Uri url;
+
+        public DriverSetup(AppiumDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void setCap(AppiumOptions capabilities)
+        {
+            cap = capabilities;
+            cap = new AppiumOptions();
+            cap.PlatformName = "Android";
+            cap.DeviceName = "emulator-5554";
+            cap.PlatformName = "9";
+            cap.App = "C:\\Users\\Gokul.Chandramohan\\Downloads\\calculator.apk";
+            url = new Uri("http://127.0.0.1:4723/wd/hub");
+        }
+
+        public AppiumDriver setDriver()
+        {
+            driver = new AndroidDriver(url, cap);
+            Thread.Sleep(4000);
+            return driver;
+        }
+
+    }
+}
+
---
+++ AppiumDemo/pageClass.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium.Appium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppiumTest
+{
+    public class pageClass
+    {
+        AppiumDriver driver;
+        public pageClass(AppiumDriver driver)
+
+        {
+            this.driver = driver;
+
+        }
+
+        public void testAction()
+        {
+            AppiumElement a = driver.FindElement(ById.Id("com.ulsumobile.calculator:id/iv_8"));
+            AppiumElement operation = driver.FindElement(ById.Id("com.ulsumobile.calculator:id/iv_cheng"));
+            AppiumElement b = driver.FindElement(ById.Id("com.ulsumobile.calculator:id/iv_9"));
+            AppiumElement res = driver.FindElement(ById.Id("com.ulsumobile.calculator:id/iv_deng"));
+            a.Click();
+            operation.Click();
+            b.Click();
+            res.Click();
+        }
+
+    }
+}
